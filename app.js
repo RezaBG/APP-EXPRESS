74fdbb74d1efd3b83491c53d1e371c0747386d55
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-04-17 07:39:44 +0200

    update 3

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const express = require("express");
+const path = require("path");
 
-const productRoutes = require("./routes/product-routes");
+const express = require("express");
 
 const app = express();
 
-app.use("/api", productRoutes);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "punlic")));
+
+app.use("admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h2>Not Found.</h2>");
+});
 
 app.listen(8000);
